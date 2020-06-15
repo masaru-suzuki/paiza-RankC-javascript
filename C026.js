181c89bf9b255632e51a1d4164de8cf722d6ad11
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-06-16 06:16:41 +0900

    C026

--- C026.js
+++ C026.js
@@ -48,14 +48,24 @@ reader.on('close', () => {
        return a.reduce((x,y) => Math.max(x,y));
    };
 //   console.log(max(tolerancedCarrot))
-  
+
+
 //   質量最大の人参の番号を出力
-  for(let j = 0; j < tolerancedCarrot.length; j++) {
-      if(tolerancedCarrot[j][1] === max(tolerancedCarrot)){
-          console.log(tolerancedCarrot[j][0]);
-          break;
-      }
+for(let j = 0; j < tolerancedCarrot.length; j++) {
+  if(tolerancedCarrot[j][1] === max(tolerancedCarrot)){
+    console.log(tolerancedCarrot[j][0]);
+    break;
   }
+}
+// //   質量最大の人参の番号を出力 [sortパターン]
+// tolerancedCarrot.sort(function(a, b) {return a[1] - b[1];});
+// const max = tolerancedCarrot[tolerancedCarrot.length -1][1];
+// for(let j = 0; j < tolerancedCarrot.length; j++) {
+//     if(tolerancedCarrot[j][1] === max){
+//         console.log(tolerancedCarrot[j][0]);
+//         break;
+//     }
+// }
 
 
 
